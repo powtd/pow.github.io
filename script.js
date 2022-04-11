e2c030a460807087de7da486e1c8011bc7971245
Phruksachat Yaogul <42583572+pperal@users.noreply.github.com>
2022-04-11 23:14:13 +0700

    add

--- script.js
+++ script.js
@@ -61,3 +61,15 @@ var randomEffect = function () {
 }
 
 //----------------------------------
+
+$("a[href^='#']").click(function (e) {
+	e.preventDefault();
+
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate(
+		{
+			scrollTop: position
+		} /* speed */
+	);
+});
